7cceedb913898e50236efe74d082ebcf5235f10d
korantshi <korantshi@hotmail.com>
2014-11-22 00:45:41 -0500

    library files for parsing xml

---
+++ myxml/myinstall/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/afs/andrew.cmu.edu/usr22/yishi/private/15441/project3/myxml/myinstall/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/afs/andrew.cmu.edu/usr22/yishi/private/15441/project3/myxml/myinstall/include/libxml2"
+MODULE_VERSION="xml2-2.7.3"
+
